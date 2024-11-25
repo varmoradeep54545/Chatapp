fefec5676376f77696ce0a820d97332a31affb52
Varmora deep <113370542+varmoradeep54545@users.noreply.github.com>
2024-11-25 23:12:53 +0530

    Update generateToken.js

--- Backend/jwt/generateToken.js
+++ Backend/jwt/generateToken.js
@@ -1,4 +1,4 @@
-const jwt = require('jsonwebtoken');
+import jwt from 'jsonwebtoken';
 
 const createTokenAndSaveCookie = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_TOKEN, {
