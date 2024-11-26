7fa57025fcab117c3555e9b1d3fbca1b2989a060
Varmora deep <113370542+varmoradeep54545@users.noreply.github.com>
2024-11-26 11:29:42 +0530

    Update SocketContext.jsx

--- Frontend/src/context/SocketContext.jsx
+++ Frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:4002", {
+      const socket = io("https://chatapp9.vercel.app/api", {
         query: {
           userId: authUser.user._id,
         },
