d4cfaff69c4f19cb2103fe4bbb541733fde3293e
Varmora deep <113370542+varmoradeep54545@users.noreply.github.com>
2024-11-26 16:42:22 +0530

    Update server.js

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -1,14 +1,13 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
-const cors = require('cors');
 
 const app = express();
 
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://chatapp-client-rouge.vercel.app",
+    origin: "http://localhost:3001 ",
     methods: ["GET", "POST"],
   },
 });
