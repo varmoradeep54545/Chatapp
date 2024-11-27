4b90292408daf1005d3c3ac1b1ddcdc5a69cbaff
Varmora deep <113370542+varmoradeep54545@users.noreply.github.com>
2024-11-27 10:22:07 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001,
     proxy: {
       "/api": {
         target: "https://chatapp9.vercel.app/api",
