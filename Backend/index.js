2dff889f8a28231574618a18d033a96db5e79183
Varmora deep <113370542+varmoradeep54545@users.noreply.github.com>
2024-11-27 10:09:53 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -15,13 +15,25 @@ const app = express();
 const server = http.createServer(app);
 
 // Socket.IO configuration
+const allowedOrigins = [
+  "https://chatapp-client-rouge.vercel.app"
+
+];
+
 const io = new Server(server, {
   cors: {
-    origin: "https://chatapp-client-rouge.vercel.app",
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true); // Allow the request
+      } else {
+        callback(new Error("Not allowed by CORS")); // Reject the request
+      }
+    },
     methods: ["GET", "POST"],
     credentials: true,
   },
 });
+
 
 // Real-time message handling
 const users = {};
